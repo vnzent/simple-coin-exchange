1f177133b4c43fbcb28d6bb206b5527dfb02037e
Vincent <vincent.ptk17@gmail.com>
2025-06-14 01:57:29 +0800

    First Commit

---
+++ me.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "Masukkan jumlah jenis koin: ";
+  cin >> n;
+
+  vector<int> coins(n);
+  for (int i = 0; i < n; i++) {
+    cout << "Masukkan nilai koin ke-" << i + 1 << ": ";
+    cin >> coins[i];
+  }
+
+  sort(coins.begin(), coins.end(), greater<int>());
+
+  int amount;
+  cout << "Masukkan jumalh uang yang ingin ditukarkan kedalam bentu koin : ";
+  cin >> amount;
+
+  vector<int> count(n, 0); // Jumlah koin dari setiap jenis koin
+  int sisa = amount;
+
+  for (int i = 0; i < n; i++) {
+    while (sisa >= coins[i]) {
+      sisa -= coins[i];
+      count[i]++;
+    }
+  } 
+
+  // Output
+  cout << "\nHasil penukaran untuk uang " << amount << " adalah:\n";
+  for (int i = 0; i < n; i++) {
+    if (count[i] > 0) {
+      cout << i + 1 << ". Koin " << coins[i] << " x " << count[i] << "\n";
+    }
+  }
+
+  if (amount > 0) {
+    cout << "Tidak dapat menukarkan sisa uang " << sisa << " dengan koin yang tersedia.\n";
+  }
+
+  return 0;
+}
